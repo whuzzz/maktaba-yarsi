3b921e2b57d05ced969b80dd7bf0b16faa01a404
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-04 20:55:41 +0700

    add & setup tailwind v3.0.18

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <p className={{}}>{data[0]?.content[5]?.text}</p>
+        <p className="bg-red-600 text-white">{data[0]?.content[5]?.text}</p>
       </main>
     </>
   );
