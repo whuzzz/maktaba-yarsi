b482bd063ed975dc0aa9b86267c6e4d0b01dc8fd
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-05 15:26:52 +0700

    downgrade tailwind version to 3.0.2 & add two custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
+  content: ['./src/pages/**/*.jsx', './src/components/**/*.{js,jsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-        mulish: "'Mulish', sans-serif",
+        poppins: 'Poppins, sans-serif',
       },
     },
   },
