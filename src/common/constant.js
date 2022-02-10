f1371eda611ad6d5326c8e38d362ffe298fb16b5
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-10 23:16:43 +0700

    fix API url

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/"
-    : "https://buku-islam-api.vercel.app";
+    : "https://buku-islam-api.vercel.app/";
 
 const API_CONFIG = {
   GET_BOOKS: `${BASE_URL}books`,
