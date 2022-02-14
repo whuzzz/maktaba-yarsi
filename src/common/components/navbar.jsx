48cd454be8183d13b580865ad1ebcfc5ac6816d5
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-14 18:53:51 +0700

    create & update navbar routes

--- src/common/components/navbar.jsx
+++ src/common/components/navbar.jsx
@@ -1,26 +1,45 @@
+import Link from "next/link";
 import { useEffect } from "react";
 import useDarkMode, { DarkModeToggler } from "use-dark-mode-hook";
+import { NAV_LIST } from "../constant";
 import { addDarkColorScheme } from "../helpers";
+import { FaGithub } from "react-icons/fa";
 
 function Navbar() {
   const [isDarkMode, toggleDarkMode] = useDarkMode({ element: "html" });
   useEffect(addDarkColorScheme, [isDarkMode, toggleDarkMode]);
 
   return (
-    <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b border-black/10 bg-white px-10 py-3.5 text-slate-700 duration-150 dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200">
+    <nav className="fixed top-0 z-50 mb-14 flex w-full items-center justify-between border-b border-black/10 bg-white px-10 py-3.5 text-slate-700 duration-150 dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200">
       <p className="text-2xl font-semibold text-green-600 dark:text-green-500">
         YASLAB
       </p>
-      <ul className="flex items-center">
-        <li>Book</li>
-        <li>
+      <div className="flex items-center space-x-8">
+        <ul className="flex items-center space-x-8 font-semibold">
+          {NAV_LIST.map(({ list }) => (
+            <Link key={list} href={`/${list}`}>
+              <a className="capitalize hover:text-green-600 dark:hover:text-green-500">
+                {list}
+              </a>
+            </Link>
+          ))}
+        </ul>
+        <div className="flex items-center border-l border-black/10 pl-6 dark:border-gray-800">
           <DarkModeToggler
             isDarkMode={isDarkMode}
             toggleDarkMode={toggleDarkMode}
             buttonClassName="dark:text-green-500 text-green-600"
           />
-        </li>
-      </ul>
+          <a
+            href="https://github.com/ReySuryanom/yaslab"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-6 text-xl text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
