9b26b3f5714fb0e3923d448a5aab57b966034938
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-25 21:42:57 +0700

    setup tailwindcss custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         poppins: 'Poppins, sans-serif',
       },
+      colors: {
+        primary: {
+          light: '#059669',
+          dark: '#10b981',
+        },
+        light: {
+          100: '#f8fafc',
+          200: '#f1f5f9',
+          300: '#e2e8f0',
+          400: '#cbd5e1',
+        },
+        dark: {
+          100: '#0f172a',
+          200: '#1e293b',
+          300: '#334155',
+          400: '#475569',
+        },
+      },
     },
   },
   plugins: [],
